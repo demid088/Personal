dcc5ba15b56ad265c88dddeea54bab40e833f0bd
Dmitry Demidov <demid088@yandex.ru>
2023-02-02 21:56:18 +0700

    [add] - animation

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,15 +1,22 @@
-import "./Home.css"
+import './Home.css'
+import './animationBg.css'
+import './animationArrow.css'
 
 function Home() {
   return (
     <section className='home' id='home'>
+      <div className='home__bg home__bg_animation'>
+        <div id='stars'></div>
+        <div id='stars2'></div>
+        <div id='stars3'></div>
+      </div>
       <div className='home__text'>
         <h1 className='home__title'>Demidov Dmitriy</h1>
         <p className='home__subtitle'>Web Developer</p>
         <button className='btn home__btn'>Contact Me</button>
       </div>
-      <div className='home__arrow'>
-        <span className='material-icons home__icon'>expand_more</span>
+      <div className='home__arrow home__arrow_animation'>
+        <a href='#about' className='material-icons home__icon'>expand_more</a>
       </div>
     </section>
   )
