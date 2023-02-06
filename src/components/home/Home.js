5cfdc48c3bbb6f1e51ab1f7fbf88c8789800e114
Dmitry Demidov <demid088@yandex.ru>
2023-02-06 20:27:42 +0700

    add scroll

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -13,10 +13,12 @@ function Home() {
       <div className='home__text'>
         <h1 className='home__title'>Demidov Dmitriy</h1>
         <p className='home__subtitle'>Web Developer</p>
-        <button className='btn home__btn'>Contact Me</button>
+        <button className='btn btn_outline home__btn'>Contact Me</button>
       </div>
       <div className='home__arrow home__arrow_animation'>
-        <a href='#about' className='material-icons home__icon'>expand_more</a>
+        <a href='#about' className='material-icons home__icon'>
+          expand_more
+        </a>
       </div>
     </section>
   )
--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,59 +1,105 @@
-import "./Menu.css"
+import { Link, animateScroll as scroll } from 'react-scroll'
+
+import './Menu.css'
 
 import logo from './img/freelancer.jpg'
+
+const scrollToTop = () => {
+  scroll.scrollToTop()
+}
 
 function Menu() {
   return (
     <nav className='menu'>
-      <a href='#home' className='menu__logo'>
+      <div onClick={scrollToTop} className='menu__logo'>
         {/* <h2 className='logo__title'>Demidov Dmitriy</h2> */}
         <div className='logo__img'>
-          <img src={logo} alt="freelancer" />
+          <img src={logo} alt='freelancer' />
         </div>
         {/* <span className='logo__subtitle'>React Developer</span> */}
-      </a>
+      </div>
       <ul className='menu__list'>
         <li className='menu__item'>
-          <a href='#home' className='menu__link _flex'>
+          <Link
+            className='menu__link _flex'
+            activeClass='active'
+            to='home'
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={500}
+          >
             <span className='material-icons menu__icon'>home</span>
             <span className='menu__text'>Home</span>
-          </a>
+          </Link>
         </li>
         <li className='menu__item'>
-          <a href='#about' className='menu__link _flex'>
+          <Link
+            className='menu__link _flex'
+            activeClass='active'
+            to='about'
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={500}
+          >
             <span className='material-icons menu__icon'>person</span>
             <span className='menu__text'>About</span>
-          </a>
+          </Link>
         </li>
         <li className='menu__item'>
-          <a href='#projects' className='menu__link _flex'>
+          <Link
+            className='menu__link _flex'
+            activeClass='active'
+            to='projects'
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={500}
+          >
             <span className='material-icons menu__icon'>space_dashboard</span>
             <span className='menu__text'>Projects</span>
-          </a>
+          </Link>
         </li>
         <li className='menu__item'>
-          <a href='#skills' className='menu__link _flex'>
+          <Link
+            className='menu__link _flex'
+            activeClass='active'
+            to='skills'
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={500}
+          >
             <span className='material-icons menu__icon'>bar_chart</span>
             <span className='menu__text'>Skills</span>
-          </a>
+          </Link>
         </li>
-        <li className='menu__item'>
+        {/* <li className='menu__item'>
           <a href='#testimonials' className='menu__link _flex'>
             <span className='material-icons menu__icon'>interpreter_mode</span>
             <span className='menu__text'>Testimonials</span>
           </a>
-        </li>
-        <li className='menu__item'>
+        </li> */}
+        {/* <li className='menu__item'>
           <a href='#blog' className='menu__link _flex'>
             <span className='material-icons menu__icon'>edit</span>
             <span className='menu__text'>Blog</span>
           </a>
-        </li>
+        </li> */}
         <li className='menu__item'>
-          <a href='#contact' className='menu__link _flex'>
+          <Link
+            className='menu__link _flex'
+            activeClass='active'
+            to='contact'
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={500}
+          >
             <span className='material-icons menu__icon'>email</span>
             <span className='menu__text'>Contact</span>
-          </a>
+          </Link>
         </li>
       </ul>
     </nav>
