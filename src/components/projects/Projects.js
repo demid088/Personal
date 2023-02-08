63b4b89311d56a1ae042e6addf16000996daaded
Dmitry Demidov <demid088@yandex.ru>
2023-02-08 21:21:16 +0700

    add card

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -6,8 +6,28 @@ function Projects() {
       <header className='projects__header'>
         <h2 className='projects__title'>PROJECTS</h2>
       </header>
-      <div className="projects__content">
-        
+      <div className='projects__content'>
+        <ul className='projects__cards'>
+          <li className='projects__card'>
+            <div className='card__bg'>
+              <img
+                src='http://static.buqara.kz/uploads/images/normal/c5c69629cc87fc55064cefcebc5ff2fc.jpg'
+                alt=''
+              />
+            </div>
+            <div className="card__hover">
+              <div className='card__text'>
+                <h4 className='card__title'>Title project</h4>
+                <span className='card__desc'>React / Node</span>
+              </div>
+              <div className='card__btn'>
+                <a href='/' className='btn btn_outline card__link'>
+                  LEARN MORE
+                </a>
+              </div>
+            </div>
+          </li>
+        </ul>
       </div>
     </section>
   )
