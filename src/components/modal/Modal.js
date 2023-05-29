fddd534eaae1d6145ebcc81402e557476b6ae5ab
Dmitry Demidov <demid088@yandex.ru>
2023-05-29 23:42:29 +0700

    [edit] - noscript & modal

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,6 +1,21 @@
+import { useEffect } from 'react'
 import './Modal.css'
 
 function Modal({ open, setOpen, children }) {
+  const keydownHandler = ({ key }) => {
+    switch (key) {
+      case 'Escape':
+        setOpen(false)
+        break
+      default:
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('keydown', keydownHandler)
+    return () => document.removeEventListener('keydown', keydownHandler)
+  })
+
   return (
     <div className={'modal-wrapper' + (open ? ' show' : '')}>
       <div className={'modal' + (open ? ' show' : '')}>
